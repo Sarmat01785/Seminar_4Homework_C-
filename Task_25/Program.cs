99bbd460110afc5fdebb3d7082ab990fddbd29d8
Sarmat <Spirator@Yandex.ru>
2023-06-22 22:18:59 +0700

    задача 25 решена

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -3,3 +3,24 @@
 2, 4 -> 16
 */
 
+int EnterTwoNumbers(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int TwoNumber(int num1, int num2)
+{
+    int degree = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        degree = degree * num1;
+    }
+    return degree;
+}
+
+
+int num1 = EnterTwoNumbers("Введите первое число: ");
+int num2 = EnterTwoNumbers("Введите второе число: ");
+
+int result = TwoNumber(num1, num2);
+Console.WriteLine(result);
