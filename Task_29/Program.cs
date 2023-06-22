4580f7bf0e2ea5bab5c1e8bf1b6e853346331e22
Sarmat <Spirator@Yandex.ru>
2023-06-22 20:16:06 +0700

    задача 29 решена

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -2,3 +2,32 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int[] ArrayRand(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0,100);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+}
+
+int len = InputNum("Введите длину массива: ");
+
+Console.Write("[");
+int[] arr = ArrayRand(len);
+PrintArray(arr);
+Console.WriteLine("]");
