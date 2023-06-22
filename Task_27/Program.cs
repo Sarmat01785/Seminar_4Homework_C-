43228eb6e444d99f7cdf90b25b06e58b9185aeb9
Sarmat <Spirator@Yandex.ru>
2023-06-22 20:58:43 +0700

    задача 27 решена

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -3,3 +3,24 @@
 82 -> 10
 9012 -> 12
 */
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int Amount(int num)
+{
+    int result = 0;
+
+    while (num > 0)
+    {
+        result = result + num % 10;   // result += num % 10;
+
+        num = num / 10;
+    }
+    return result;
+}
+
+int number = InputNum(" Введите целое число: ");
+int result = Amount(number);
+Console.Write(" Суммма цифр в числе: " + result);
